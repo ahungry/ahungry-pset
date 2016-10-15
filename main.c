864b553535b4dd3e39cfb7b1dc6f65cae3e61dc1
Matthew Carter <m@ahungry.com>
2016-10-15 00:10:52 -0400

    Update comments

--- main.c
+++ main.c
@@ -1,16 +1,35 @@
-// http://boredzo.org/pointers/
+// Ahungry Pset - A potential PHP extension (maybe...)
+// Copyright (C) 2016 Matthew Carter
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY/ without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * Better way to track our foo.type than comparing strings
+ */
 enum
   {
     SCALAR,
     LIST
   };
 
-// Each thing inside here is a member
+/**
+ * Type allows us to have named keys, and values or lists
+ */
 typedef struct foo
 {
   short type;
@@ -19,6 +38,13 @@ typedef struct foo
   struct foo *list[5];
 } foo;
 
+/**
+ * Recurse across the struct and format contents
+ *
+ * @param foo* foo_ptr The pointer to the foo structure
+ * @param int nest The counter for how far to indent
+ * @return void
+ */
 void dump (foo* foo_ptr, int nest)
 {
   int i;
@@ -42,12 +68,20 @@ void dump (foo* foo_ptr, int nest)
     }
 }
 
+/**
+ * Do we need to describe this?
+ *
+ * @param int argc How many arguments passed in
+ * @param char *argv[] The array of string arguments
+ * @return int Success (0) or failure (!0)
+ */
 int main (int argc, char *argv[])
 {
   printf ("Running %s, We saw %d args...\n", argv[0], argc);
-
   printf ("Scalar: %d, List: %d", SCALAR, LIST);
 
+  // Set up the equivalent of the following PHP array:
+  // $arr = ['a' => [1, 2], 'b' => [3, 4]]
   foo a2 = { SCALAR, "\0", "2", { NULL } };
   foo a1 = { SCALAR, "\0", "1", { NULL } };
   foo b2 = { SCALAR, "\0", "4", { NULL } };
@@ -59,7 +93,6 @@ int main (int argc, char *argv[])
   printf ("k: %s, v: %s\n", foo1.list[0]->key, foo1.list[0]->val);
 
   foo *foo_ptr = &foo1;
-  //foo_ptr = foo_ptr->list[0];
 
   printf ("Its loopin time\n");
   dump (foo_ptr, 0);
